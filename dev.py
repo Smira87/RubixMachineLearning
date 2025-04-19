b9e79b6b29f90e5329b3d1c021c98558e4d2a5c5
Serhii Smirnov <snoff87@gmail.com>
2025-04-19 19:21:26 +0300

    cube piece not rendering

--- dev.py
+++ dev.py
@@ -1,7 +1,11 @@
 import pyray as pr
 import configs
+from rubik import Rubik
 
 pr.init_window(configs.window_w, configs.window_h, "Building Rubik's Cube")
+
+rubik_cube = Rubik()
+piece = rubik_cube.generate_rubik(size=1)
 
 pr.set_target_fps(configs.fps)
 
@@ -12,9 +16,12 @@ while not pr.window_should_close():
     pr.begin_drawing()
     pr.clear_background(pr.RAYWHITE)
 
+    position = pr.Vector3(piece.center[0], piece.center[1], piece.center[2] )
+    pr.draw_model(piece.model, position, 2, piece.face_color)
     pr.begin_mode_3d(configs.camera)
-
+    pr.draw_grid(20, 1.0)
     pr.end_mode_3d()
+
     pr.end_drawing()
 
 pr.close_window()
