a0a5e4c47acef063d09fe66ed822b95955d96570
Serhii Smirnov <snoff87@gmail.com>
2025-05-23 23:56:58 +0300

    lesson 2_fixed

--- rubik.py
+++ rubik.py
@@ -26,9 +26,9 @@ class Rubik:
     def generate_rubik(self, size):
         colors = [pr.WHITE, pr.BLUE, pr.RED, pr.GREEN, pr.YELLOW, pr.ORANGE]
         offset = size - 0.7
-        size_x = size * 0.1, size * 0.9, size * 0.9
-        size_y = size * 0.9, size * 0.1, size * 0.9
         size_z = size * 0.9, size * 0.9, size * 0.1
+        size_x = size * 0.9, size * 0.1, size * 0.9
+        size_y = size * 0.1, size * 0.9, size * 0.9
 
         for x in range(1):
             for y in range(1):
